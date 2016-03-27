54c8c34b50fb86f78a508dcb75927f69c25892fd
ikasparov <ikasparov.dev@gmail.com>
2016-03-27 10:43:40 +0300

    Модифицирован синтаксис

--- app/frontend/common/services/current_user.ts
+++ app/frontend/common/services/current_user.ts
@@ -1,26 +1,33 @@
 "use strict";
+
 import User from "./user";
 
 export default class CurrentUser extends User {
+
     public static $inject: string[] = ["$http", "$rootScope"];
     public activities: string[];
+
     constructor(
         private $http: ng.IHttpService,
-        private $rootScope: IAppRootScope
-    ) {
+        private $rootScope: IAppRootScope) {
+
         super();
     }
 
     public load(): ng.IPromise<CurrentUser> {
+
         let scope: CurrentUser = this;
-        return this.$http.get("/api/v1/users/current").success(function(response: any): IUser {
-            angular.extend(scope, response['user']);
-            scope.$rootScope.currentUser = scope;
-            return scope;
-        });
+
+        return this.$http.get("/api/v1/users/current")
+            .success((response: Object): IUser => {
+                angular.extend(scope, response["user"]);
+                scope.$rootScope.currentUser = scope;
+                return scope;
+            });
     }
 
     public can(activity: string): boolean {
+
         return this.activities.indexOf(activity) >= 0;
     }
 }
