543bc0961e39ecce3b5246c871e85f537f3e17ee
ikasparov <ikasparov.dev@gmail.com>
2016-06-16 10:18:06 +0300

    Рефакторинг основной точки входа клиентского приложения app.ts. refs #15

--- app/frontend/app.ts
+++ app/frontend/app.ts
@@ -9,31 +9,36 @@ import "./assets/template/icons/flags/flags.css";
 import "./assets/template/stylesheets/main.css";
 import "./app.scss";
 
-import RootConfig   from "./config";
+import RootConfig from "./config";
 import RoutesConfig from "./routes";
-import common       from "./common/index";
-import core         from "./core/index";
 
+import "./common";
+import "./core";
 import "./companies";
+import "./events";
+
+let dependencies: Array<string> = [
+    "ui.router",
+    "ngSanitize",
+    "ngAnimate",
+    "ngRetina",
+    "ncy-angular-breadcrumb",
+    "companies",
+    "events",
+    "common",
+    "core"
+];
 
 angular
-    .module("trucksApp", [
-        "ui.router",
-        "ngSanitize",
-        "ngAnimate",
-        "ngRetina",
-        "ncy-angular-breadcrumb",
-        "companies",
-        common,
-        core])
+    .module("trucksApp", dependencies)
     .run(RootConfig)
     .config(RoutesConfig)
-    .config(["$httpProvider", ($httpProvider: any): void => {
+    .config(["$httpProvider", ($httpProvider: ng.IHttpProvider): void => {
         $httpProvider.defaults.headers.common = {};
         $httpProvider.defaults.headers.post = { "Content-Type": "application/json" };
         $httpProvider.defaults.headers.put = { "Content-Type": "application/json" };
         $httpProvider.defaults.headers.patch = { "Content-Type": "application/json" };
 
-        let authToken: string = $("meta[name=\"csrf-token\"]").attr("content");
-        $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+        // let authToken: string = $("meta[name=\"csrf-token\"]").attr("content");
+        // $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
     }]);
